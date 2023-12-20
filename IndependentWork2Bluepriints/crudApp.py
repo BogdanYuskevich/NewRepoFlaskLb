c7e847b746e8a9647fa2b9c650c5f7f336b7f11c
Bohdan <yuskevich12@gmail.com>
2023-12-20 18:07:38 +0200

    add independent work 2 (test)

--- IndependentWork2Bluepriints/crudApp.py
+++ IndependentWork2Bluepriints/crudApp.py
@@ -1,152 +1,153 @@
-from flask import Flask, Blueprint, request, jsonify, make_response
+from flask import Flask, Blueprint, request, jsonify
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-import uuid
-import jwt
-import datetime
-from functools import wraps
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///games.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'Hello'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
+
+users_bp = Blueprint('users', __name__)
+games_bp = Blueprint('games', __name__)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    public_id = db.Column(db.String(50), unique=True)
-    name = db.Column(db.String(50))
-    password = db.Column(db.String(80))
-    admin = db.Column(db.Boolean)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = generate_password_hash(password)
 
 
-api = Blueprint('api', __name__)
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    genre = db.Column(db.String(50), nullable=False)
+    platform = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, title, genre, platform):
+        self.title = title
+        self.genre = genre
+        self.platform = platform
 
 
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-
-        if not token:
-            return jsonify({'message': 'Token is missing!'}), 401
-
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-            current_user = User.query.filter_by(public_id=data['public_id']).first()
-        except:
-            return jsonify({'message': 'Token is invalid!'}), 401
-
-        return f(current_user, *args, **kwargs)
-
-    return decorated
-
-
-@api.route('/user', methods=['GET'])
-@token_required
-def get_all_users(current_user):
-    if not current_user.admin:
-        return jsonify({'message': 'Cannot perform that function!'})
-
-    users = User.query.all()
-
-    output = []
-
-    for user in users:
-        user_data = {'public_id': user.public_id, 'name': user.name, 'password': user.password, 'admin': user.admin}
-        output.append(user_data)
-
-    return jsonify({'users': output})
-
-
-@api.route('/user/<public_id>', methods=['GET'])
-@token_required
-def get_one_user(current_user, public_id):
-    if not current_user.admin:
-        return jsonify({'message': 'Cannot perform that function!'})
-
-    user = User.query.filter_by(public_id=public_id).first()
-
-    if not user:
-        return jsonify({'message': 'No user found!'})
-
-    user_data = {'public_id': user.public_id, 'name': user.name, 'password': user.password, 'admin': user.admin}
-
-    return jsonify({'user': user_data})
-
-
-@api.route('/user', methods=['POST'])
-def create_user():
+@app.route('/auth/register', methods=['POST'])
+def register():
     data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
 
-    hashed_password = generate_password_hash(data['password'], method='sha256')
+    if not username or not password:
+        return jsonify({'message': 'Username and password are required'}), 400
 
-    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
+    if User.query.filter_by(username=username).first():
+        return jsonify({'message': 'Username already exists'}), 400
+
+    new_user = User(username=username, password=password)
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({'message': 'New user created!'})
+    return jsonify({'message': 'User registered successfully'}), 201
 
 
-@api.route('/user/<public_id>', methods=['PUT'])
-@token_required
-def promote_user(current_user, public_id):
-    if not current_user.admin:
-        return jsonify({'message': 'Cannot perform that function!'})
-
-    user = User.query.filter_by(public_id=public_id).first()
-
-    if not user:
-        return jsonify({'message': 'No user found!'})
-
-    user.admin = True
-    db.session.commit()
-
-    return jsonify({'message': 'The user has been promoted!'})
-
-
-@api.route('/user/<public_id>', methods=['DELETE'])
-@token_required
-def delete_user(current_user, public_id):
-    if not current_user.admin:
-        return jsonify({'message': 'Cannot perform that function!'})
-
-    user = User.query.filter_by(public_id=public_id).first()
-
-    if not user:
-        return jsonify({'message': 'No user found!'})
-
-    db.session.delete(user)
-    db.session.commit()
-
-    return jsonify({'message': 'The user has been deleted!'})
-
-
-@app.route('/login', methods=['POST'])
+@users_bp.route('/auth/login', methods=['POST'])
 def login():
-    auth = request.authorization
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
 
-    if not auth or not auth.username or not auth.password:
-        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+    user = User.query.filter_by(username=username).first()
 
-    user = User.query.filter_by(name=auth.username).first()
+    if not user or not check_password_hash(user.password, password):
+        return jsonify({'message': 'Invalid username or password'}), 401
 
-    if not user:
-        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+    access_token = create_access_token(identity=username)
+    return jsonify({'access_token': access_token}), 200
 
-    if check_password_hash(user.password, auth.password):
-        token = jwt.encode(
-            {'public_id': user.public_id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
-            app.config['SECRET_KEY'])
-        return jsonify({'token': token.decode('UTF-8')})
 
-    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+@games_bp.route('/games', methods=['GET'])
+@jwt_required()
+def get_games():
+    games = Game.query.all()
+    result = []
+    for game in games:
+        game_data = {
+            'id': game.id,
+            'title': game.title,
+            'genre': game.genre,
+            'platform': game.platform
+        }
+        result.append(game_data)
+    return jsonify(result), 200
+
+
+def add_game():
+    data = request.get_json()
+    title = data.get('title')
+    genre = data.get('genre')
+    platform = data.get('platform')
+
+    if not title or not genre or not platform:
+        return jsonify({'message': 'Title, genre, and platform are required'}), 400
+
+    new_game = Game(title=title, genre=genre, platform=platform)
+    db.session.add(new_game)
+    db.session.commit()
+
+    return jsonify({'message': 'Game added successfully'}), 201
+
+
+@games_bp.route('/games/<int:game_id>', methods=['GET'])
+@jwt_required()
+def get_game(game_id):
+    game = Game.query.get(game_id)
+    if game:
+        game_data = {
+            'id': game.id,
+            'title': game.title,
+            'genre': game.genre,
+            'platform': game.platform
+        }
+        return jsonify(game_data), 200
+    else:
+        return jsonify({'message': 'Game not found'}), 404
+
+
+@games_bp.route('/games/<int:game_id>', methods=['PUT'])
+@jwt_required()
+def update_game(game_id):
+    game = Game.query.get(game_id)
+    if game:
+        data = request.get_json()
+        game.title = data.get('title', game.title)
+        game.genre = data.get('genre', game.genre)
+        game.platform = data.get('platform', game.platform)
+
+        db.session.commit()
+        return jsonify({'message': 'Game updated successfully'}), 200
+    else:
+        return jsonify({'message': 'Game not found'}), 404
+
+
+@games_bp.route('/games/<int:game_id>', methods=['DELETE'])
+@jwt_required()
+def delete_game(game_id):
+    game = Game.query.get(game_id)
+    if game:
+        db.session.delete(game)
+        db.session.commit()
+        return jsonify({'message': 'Game deleted successfully'}), 200
+    else:
+        return jsonify({'message': 'Game not found'}), 404
 
 
 if __name__ == '__main__':
+    db.create_all()
+    app.register_blueprint(games_bp)
     app.run(debug=True)
